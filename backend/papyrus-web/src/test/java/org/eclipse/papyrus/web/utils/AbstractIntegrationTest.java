218de87fcc64f55c8f2b8b21186ee840ad0fae92
Vincent Lorenzo <vincent.lorenzo@cea.fr>
2025-09-24 14:42:07 +0000

    GL-256 Activate GitLab runners to build Papyrus-Web and create a docker file -build -allTests execution -usage of TestContainer configured with a boolean value and unactivated for the GitLab build (TestContainer are not available on Eclipse GitLab)+ -maven publication
    Issue https://gitlab.eclipse.org/eclipse/papyrus/org.eclipse.papyrus-web/-/issues/256
    Signed-off-by: Marcos DIDONET DEL FABRO <marcos.didonetdelfabro@cea.fr>

--- backend/papyrus-web/src/test/java/org/eclipse/papyrus/web/utils/AbstractIntegrationTest.java
+++ backend/papyrus-web/src/test/java/org/eclipse/papyrus/web/utils/AbstractIntegrationTest.java
@@ -1,5 +1,5 @@
 /*****************************************************************************
- * Copyright (c) 2024 CEA LIST.
+ * Copyright (c) 2024, 2025 CEA LIST.
  *
  * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License 2.0
@@ -10,40 +10,79 @@
  *
  * Contributors:
  *  Obeo - Initial API and implementation
+ *  Marcos Didonet Del Fabro (CEA LIST) - Issue 256
  *******************************************************************************/
 package org.eclipse.papyrus.web.utils;
 
 import org.eclipse.sirius.web.infrastructure.configuration.persistence.JDBCConfiguration;
 import org.eclipse.sirius.web.starter.SiriusWebStarterConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 /**
  * Abstract test class for testing web UML features.
  *
  * @author Arthur Daussy
+ * @author Marcos Didonet Del Fabro
  */
 @SpringJUnitConfig(classes = { IntegrationTestConfiguration.class, SiriusWebStarterConfiguration.class, JDBCConfiguration.class })
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class AbstractIntegrationTest {
 
-    // Emulate a POSTGRESQL database.
-    public static final PostgreSQLContainer<?> POSTGRESQL_CONTAINER;
+    // Emulate a POSTGRESQL database using testcontainers
+    private static PostgreSQLContainer<?> postgreSQLContainer;
+
+    @Value("${papyrusweb.integration.tests.datasource.url}")
+    private String datasourceUrl;
+
+    @Value("${papyrusweb.integration.tests.datasource.username}")
+    private String datasourceUsername;
+
+    @Value("${papyrusweb.integration.tests.datasource.password}")
+    private String datasourcePassword;
 
     static {
-        POSTGRESQL_CONTAINER = new PostgreSQLContainer<>("postgres:latest").withReuse(true);
-        POSTGRESQL_CONTAINER.start();
+        if (getTestContainersUse()) {
+            postgreSQLContainer = new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest")).withReuse(true);
+            postgreSQLContainer.start();
+        } else {
+            postgreSQLContainer = null;
+        }
     }
 
-    // Plug emulated database in application
+    private static Boolean getTestContainersUse() {
+        String testContainersUse = System.getProperty("papyrusweb.testcontainers.use");
+        return (testContainersUse == null) || Boolean.valueOf(testContainersUse);
+    }
+
     @DynamicPropertySource
     public static void registerProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRESQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRESQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", POSTGRESQL_CONTAINER::getPassword);
+        if (postgreSQLContainer != null) {
+            registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+            registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+            registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        } else {
+            AbstractIntegrationTest integrationTestInstance = new AbstractIntegrationTest();
+            registry.add("spring.datasource.url", integrationTestInstance::getDatasourceUrl);
+            registry.add("spring.datasource.username", integrationTestInstance::getDatasourceUsername);
+            registry.add("spring.datasource.password", integrationTestInstance::getDatasourcePassword);
+        }
     }
 
+    protected String getDatasourceUrl() {
+        return this.datasourceUrl;
+    }
+
+    protected String getDatasourceUsername() {
+        return this.datasourceUsername;
+    }
+
+    protected String getDatasourcePassword() {
+        return this.datasourcePassword;
+    }
 }
